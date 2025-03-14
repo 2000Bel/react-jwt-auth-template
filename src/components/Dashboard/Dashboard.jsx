f5c6c90a323680f3334b6367fb2accb8e661a3db
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-14 17:26:42 -0400

    dashboard shows userlist now wowwww

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,18 +1,43 @@
 // src/components/Dashboard/Dashboard.jsx
 
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { UserContext } from '../../contexts/UserContext';
+import * as userService from '../../services/userService.js';
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
+  const [userList, setUserList] = useState([]);
+
+  // first arg: function, call function
+  // second arg:
+    useEffect(() => {
+        // function
+        const fetchUsers = async() => {
+            try {
+                const fetchedUsers = await userService.index();
+                console.log(fetchedUsers);
+                setUserList(fetchedUsers);
+                
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        // calling the function
+        if (user) fetchUsers();
+        // dependency..?
+    }, [user]);
 
   return (
     <main>
       <h1>Welcome, {user.username}</h1>
-      <p>
-        This is the dashboard page where you can see a list of all the users.
-      </p>
+        <ul>
+            {userList.map((user) => (
+                <li key={user._id}>
+                    {user.username}
+                </li>
+            ))}
+        </ul>
     </main>
   );
 };
