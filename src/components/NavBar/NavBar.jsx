1b703977f6f032787067d88190d7542a8ef2b531
Matt Radford <quinquex@gmail.com>
2025-03-14 12:01:59 -0700

    maybe wrong - first part of useContext

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,16 @@
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
 import { Link } from 'react-router';
 
 const NavBar = () => {
-    return (
-      <nav>
-        <li><Link to='/sign-up'>Sign Up</Link></li>
-      </nav>
-    );
-  };
-  
-  export default NavBar;
-  
-  
+
+  const { user } = useContext(UserContext);
+
+  return (
+    <nav>
+      <li><Link to='/sign-up'>Sign Up</Link></li>
+    </nav>
+  );
+};
+
+export default NavBar;
