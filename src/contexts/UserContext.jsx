bb5270b080c01955f8d63c9119e74c2b8b666158
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-14 15:50:12 -0400

    local storage info persists

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -2,9 +2,18 @@ import { createContext, useState } from 'react';
 
 const UserContext = createContext();
 
+const getUserFromToken = () => {
+    const token = localStorage.getItem('token');
+
+    if (!token) return null;
+    // console.log(token.split('.')[1]);
+    // puts data into payload object?
+    return JSON.parse(atob(token.split('.')[1])).payload;
+};
+
 function UserProvider({ children }) {
 
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(getUserFromToken());
 
     const value = { user, setUser };
 
